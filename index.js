b75bc26ada31e68a72e260cb1e2a8ee6e31f4b40
Armanul144894 <armanul15-2842@diu.edu.bd>
2022-11-08 15:00:32 +0600

    install dotenv and jwt

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
