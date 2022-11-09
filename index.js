25996268adb0118013d35d958e9669db7209a019
Armanul144894 <armanul15-2842@diu.edu.bd>
2022-11-10 00:11:52 +0600

    get review by id

--- index.js
+++ index.js
@@ -48,9 +48,23 @@ async function run() {
       const reviews = await cursor.toArray();
       res.send(reviews);
     });
+
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await reviewCollection.findOne(query);
+      res.send(service);
+    });
     app.post("/reviews", async (req, res) => {
       const order = req.body;
       const result = await reviewCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
